a0cbc2d7b842b94db039504e0b8a802bbe506c9d
hotoo <hotoo.cn@gmail.com>
2014-09-02 17:39:12 +0800

    Update documentation

--- index.js
+++ index.js
@@ -99,25 +99,62 @@ Image.off = function(eventName, handler){
 var doc = document;
 var createElement = doc.createElement;
 
-HTMLImageElement.prototype.on = function(eventName, handler){
-  this._evt.on(eventName, handler, this);
-  return this;
-};
-HTMLImageElement.prototype.off = function(eventName, handler){
-  this._evt.off(eventName, handler, this);
-  return this;
-};
-
 doc.createElement = function(tagName){
 
-  var element = createElement.call(doc, tagName);
-  var src;
+  tagName = String(tagName).toUpperCase();
 
-  if (tagName && (tagName = String(tagName).toUpperCase()) === "IMG") {
+  var isImage = tagName === "IMG";
 
-    element._evt = new Events();
-    element.__defineSetter__("src", setter);
-    element.__defineGetter__("src", getter);
+  var element = createElement.call(doc, isImage ? "XMB" : tagName);
+
+  if (isImage) {
+
+    var image = element._image = new Image(1,1);
+    element.on = function(eventName, handler){
+      image.on.call(image, eventName, handler);
+      return element;
+    };
+    element.off = function(eventName, handler){
+      image.off.call(image, eventName, handler);
+      return element;
+    };
+    //element.on = image.on.bind(image);
+    //element.off = image.off.bind(image);
+
+    element.__defineSetter__("src", function(value){
+      this._image.src = value;
+    });
+    element.__defineGetter__("src", function(value){
+      return this._image.src;
+    });
+
+    element.__defineSetter__("status", function(value){
+      this._image.status = value;
+    });
+    element.__defineGetter__("status", function(value){
+      return this._image.status;
+    });
+
+    element.__defineSetter__("onload", function(value){
+      this._image.onload = value;
+    });
+    element.__defineGetter__("onload", function(value){
+      return this._image.onload;
+    });
+
+    element.__defineSetter__("onerror", function(value){
+      this._image.onerror = value;
+    });
+    element.__defineGetter__("onerror", function(value){
+      return this._image.onerror;
+    });
+
+    element.__defineSetter__("onabort", function(value){
+      this._image.onabort = value;
+    });
+    element.__defineGetter__("onabort", function(value){
+      return this._image.onabort;
+    });
 
   }
 
